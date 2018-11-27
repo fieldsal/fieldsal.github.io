1c765df36e123a6a69299a2fd76292a8b089c395
fieldsal <32303026+fieldsal@users.noreply.github.com>
2018-11-27 15:31:59 +0000

    fieldsal

--- graph.js
+++ graph.js
@@ -21,6 +21,9 @@ function plot(data){
 		name[i]= data[i].repo_name;
 		commits[i]= data[i].commits;
 	}
+	var width = 600;
+	var height = 600; 
+    		console.log(data);
          var svg = d3.select("svg"),
             width = svg.attr("width"),
             height = svg.attr("height"),
@@ -29,10 +32,10 @@ function plot(data){
          var g = svg.append("g")
             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-         var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+         var color = d3.scaleOrdinal var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
          
          var pie = d3.pie().value(function(d) { 
-            return d.percent; 
+            return d.commits; 
          });
          
          var path = d3.arc()
@@ -41,50 +44,24 @@ function plot(data){
          var label = d3.arc()
             .outerRadius(radius).innerRadius(radius - 80);
          
-
-         var svg = d3.select("svg"),
-            width = svg.attr("width"),
-            height = svg.attr("height"),
-            radius = Math.min(width, height) / 2;
-        
-         var g = svg.append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-         var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-         
-         var pie = d3.pie().value(function(d) { 
-            return d.percent; 
-         });
-         
-         var path = d3.arc()
-            .outerRadius(radius - 10).innerRadius(0);
-        
-         var label = d3.arc()
-            .outerRadius(radius).innerRadius(radius - 80);
-         
+         var arcAttrs = {
+          cx: function(d) { return xScale(d.x); },
+          cy: function(d) { return yScale(d.y); }
+		 };
             
             var arc = g.selectAll(".arc")
                .data(pie(data))
                .enter()
                .append("g")
-               .attr("class", "arc");
+               .attr("class", "arc",arcAttrs)
+			   .on("mouseover", handleMouseOver)
+			   .on("mouseout", handleMouseOut);
             
             arc.append("path")
                .attr("d", path)
-               .attr("fill", function(d) { return color(d.data.states); });
+               .attr("fill", function(d) { return color(d.data.repo_name); });
         
             console.log(arc)
-        
-            
-            
-            .text(function(d) { return d.data.states; });
-         
-         
-        
-         svg.append("g")
-            .attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
-            .append("text").text("Commits Pie")
-            .attr("class", "title")
 }
 
 
